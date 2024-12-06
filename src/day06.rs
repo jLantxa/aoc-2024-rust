93cb007d3963a2b8282fe4bace9178b046685cc6
Javier Lancha Vázquez <javier.lancha@gmail.com>
2024-12-06 17:13:49 +0100

    Use HashSet for the guard visited positions
    This will be useful for checking the obstacles at part 2.

--- src/day06.rs
+++ src/day06.rs
@@ -1,14 +1,14 @@
+use std::collections::HashSet;
+
 const INPUT_FILE: &str = "input/day06.txt";
 
 type Direction = (i32, i32);
 
-const CELL_EMPTY: char = '.';
 const CELL_OBSTACLE: char = '#';
 const CELL_GUARD_UP: char = '^';
 const CELL_GUARD_DOWN: char = 'v';
 const CELL_GUARD_LEFT: char = '<';
 const CELL_GUARD_RIGHT: char = '>';
-const CELL_GUARD_VISITED: char = 'X';
 
 const UP: Direction = (0, -1);
 const RIGHT: Direction = (1, 0);
@@ -44,18 +44,6 @@ impl Map {
         self.block[j as usize][i as usize]
     }
 
-    fn set(&mut self, i: i32, j: i32, value: char) {
-        self.block[j as usize][i as usize] = value;
-    }
-
-    fn rows(&self) -> std::slice::Iter<'_, Vec<char>> {
-        self.block.iter()
-    }
-
-    fn rows_mut(&mut self) -> std::slice::IterMut<'_, Vec<char>> {
-        self.block.iter_mut()
-    }
-
     fn dimensions(&self) -> (i32, i32) {
         (self.block[0].len() as i32, self.block.len() as i32)
     }
@@ -63,6 +51,45 @@ impl Map {
     fn is_inside(&self, i: i32, j: i32) -> bool {
         let dimensions = self.dimensions();
         (i >= 0 && i < dimensions.0) && (j >= 0 && j < dimensions.1)
+    }
+
+    fn get_visited_positions(&self) -> HashSet<(i32, i32)> {
+        let mut visited_positions = HashSet::new();
+        let (mut pos, mut dir) = self.find_guard().expect("Expected a guard");
+
+        loop {
+            visited_positions.insert((pos.0, pos.1));
+
+            let next_pos = (pos.0 + dir.0, pos.1 + dir.1);
+            if !self.is_inside(next_pos.0, next_pos.1) {
+                break;
+            } else if self.at(next_pos.0, next_pos.1) == CELL_OBSTACLE {
+                dir = turn_right(&dir);
+            }
+
+            pos.0 += dir.0;
+            pos.1 += dir.1;
+        }
+
+        visited_positions
+    }
+
+    fn find_guard(&self) -> Option<((i32, i32), Direction)> {
+        for (j, row) in self.block.iter().enumerate() {
+            for (i, _) in row.iter().enumerate() {
+                let ch = self.at(i as i32, j as i32);
+                let pos = (i as i32, j as i32);
+                match ch {
+                    CELL_GUARD_UP => return Some((pos, UP)),
+                    CELL_GUARD_RIGHT => return Some((pos, RIGHT)),
+                    CELL_GUARD_DOWN => return Some((pos, DOWN)),
+                    CELL_GUARD_LEFT => return Some((pos, LEFT)),
+                    _ => (),
+                }
+            }
+        }
+
+        None
     }
 }
 
@@ -72,74 +99,12 @@ fn main() {
     let map: Map = Map::from_file(INPUT_FILE).expect("Could not read the input");
 
     // Part 1
-    let num_guard_positions = calculate_guard_positions(&map);
-    println!("[Part 1] {}", num_guard_positions);
+    let visited_positions = map.get_visited_positions();
+    println!("[Part 1] {}", visited_positions.len());
 }
 
 fn turn_right(dir: &Direction) -> Direction {
     (-dir.1, dir.0)
-}
-
-fn turn_left(dir: &Direction) -> Direction {
-    (dir.1, -dir.0)
-}
-
-fn annotate_guard_positions(map: &Map) -> Map {
-    let mut annotated_map = map.clone();
-    let (mut pos, mut dir) = find_guard(map).expect("Expected a guard");
-
-    loop {
-        annotated_map.set(pos.0, pos.1, CELL_GUARD_VISITED);
-
-        let next_pos = (pos.0 + dir.0, pos.1 + dir.1);
-        if !annotated_map.is_inside(next_pos.0, next_pos.1) {
-            break;
-        } else if annotated_map.at(next_pos.0, next_pos.1) == CELL_OBSTACLE {
-            dir = turn_right(&dir);
-        }
-
-        pos.0 += dir.0;
-        pos.1 += dir.1;
-    }
-
-    annotated_map
-}
-
-fn find_guard(map: &Map) -> Option<((i32, i32), Direction)> {
-    for (j, row) in map.rows().enumerate() {
-        for (i, col) in row.iter().enumerate() {
-            let ch = map.at(i as i32, j as i32);
-            let pos = (i as i32, j as i32);
-            match ch {
-                CELL_GUARD_UP => return Some((pos, UP)),
-                CELL_GUARD_RIGHT => return Some((pos, RIGHT)),
-                CELL_GUARD_DOWN => return Some((pos, DOWN)),
-                CELL_GUARD_LEFT => return Some((pos, LEFT)),
-                _ => (),
-            }
-        }
-    }
-
-    None
-}
-
-fn count_guard_positions(annotated_map: &Map) -> u32 {
-    let mut count = 0;
-
-    for row in annotated_map.rows() {
-        for cell in row {
-            if *cell == CELL_GUARD_VISITED {
-                count += 1;
-            }
-        }
-    }
-
-    count
-}
-
-fn calculate_guard_positions(map: &Map) -> u32 {
-    let annotated_map = annotate_guard_positions(map);
-    count_guard_positions(&annotated_map)
 }
 
 #[test]
@@ -156,5 +121,7 @@ fn test_example() {
                        ......#...\n\
                       ";
     let map = Map::from_string(MAP).expect("Could not read map");
-    assert_eq!(calculate_guard_positions(&map), 41);
+
+    let visited_positions = map.get_visited_positions();
+    assert_eq!(visited_positions.len(), 41);
 }
